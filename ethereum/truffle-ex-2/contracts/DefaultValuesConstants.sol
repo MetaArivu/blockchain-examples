755b7c20a8a77afbbcffbdd981773bf932b825cf
Araf Karsh Hamid <araf.karsh@metamagic.in>
2022-06-09 00:54:35 +0530

    Updated Solidity Language Examples

--- ethereum/truffle-ex-2/contracts/DefaultValuesConstants.sol
+++ ethereum/truffle-ex-2/contracts/DefaultValuesConstants.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-// Default Values / Constants
+// Default Values / Constants / Immutable
 contract DefaultValuesConstants {
+    // Default Value
     uint public uintValue;      // 0
     int public intValue;        // 0
     bool public boolValue;      // false
@@ -11,22 +12,26 @@ contract DefaultValuesConstants {
     bytes32 public b32;         // 0x0000000000000000000000000000000000000000000000000000000000000000
 
     string public myPlace2 = "Kochi";
-}
-// Use Constants for State Variables where ever Possible
-contract GasCostWithConstants {
-    // 21420 gas 
-    // Declare state variable as Constant to save Gas
-    address public constant SENDER = 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1;
-    // 21819 gas
+
+    // 21885 gas
     // String 
     string public constant PLACE = "Kochi";
-}
-// Gas cost is very High without Constants
-contract GasCostWithVariables {
-    // 23553 gas
-    // Declare state variable as Constant to save Gas
-    address public sender = 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1;
-    // 24519 gas
+    // 24541 gas
     // String 
     string public place = "Kochi";
+
+    // Gas cost is very High without Constants
+    // 23667 gas
+    // Declare state variable as Constant to save Gas
+    address public sender = 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1;
+
+    // Gas Cost is less when you use Constant 
+    // 21509 gas 
+    // Declare state variable as Constant to save Gas
+    address public constant SENDER = 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1;
+
+    // Gas Cost is very less for immmutable State Variable
+    // 21442 gas
+    // Immutable State variable - Initialized ONLY ONCE when its deployed
+    address public immutable owner = 0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1;
 }
--- ethereum/truffle-ex-2/contracts/VariablesFunctions.sol
+++ ethereum/truffle-ex-2/contracts/VariablesFunctions.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-/**
- * State / Local / Global Variables
- * Functions View and Pure
- */
+ // State / Local / Global Variables
+ // Functions View and Pure, Fallback and receive functions
 contract VariablesFunctions {
+  // visibility
+  // ----------------------------------------------------------------------
+  // private  - ONLY INSIDE the Contract
+  // internal - ONLY INSIDE the Contract and CHILD Contracts
+  // public   - INSIDE & OUTSIDE the Contract
+  // external - ONLY OUTSIDE the Contract
+  // ----------------------------------------------------------------------
+
   // State variable are stored in the Blockchain
   string private hello = "Hello World - Truffle - Ex-2";
   uint public specialNumber = 21;
@@ -15,7 +21,6 @@ contract VariablesFunctions {
       string memory hello2 = "Local Hello";
       return (hello, hello2);
   }
-
   // Global Variables - Part of Blockchain Data
   function globalVars() external view returns (address, uint, uint) {
       address sender = msg.sender;
@@ -23,14 +28,33 @@ contract VariablesFunctions {
       uint number = block.number;
       return (sender, time, number);
   }
-
   // External and Pure (Read only) Calls for local variables
   function add(uint x, uint y) external pure returns(uint) {
     return x+y;
   }
-
   // External and View as it reads data from state variable
   function sub(uint y) external view returns(uint) {
     return specialNumber-y;
   }
+  // Event
+  event Log(string func, address sender,  uint value, bytes data);
+  // Fallback
+  fallback() external payable {
+      emit Log("fallback", msg.sender, msg.value, msg.data);
+  }
+  // Receive
+  receive() external payable {
+      emit Log("receive", msg.sender, msg.value, "");
+  }
+  /**
+     Ether is sent to Contract
+                    |
+            is msg.data is Empty?
+                   /  \
+                 Yes    No
+     receive() exists?   fallback()
+            /  \
+         Yes    No
+    receive()    fallback()
+   */
  }
